107fc84c44c4295369c888b48bf55c4d21728db8
Gopala Agrawal <agrawalgopala@gmail.com>
2024-12-29 21:48:05 +0530

    setup: inital set up

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import cors from "cors";
 import dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
 import errorHandler from "./middleware/error";
 import notFound from "./middleware/notFound";
-
 
 dotenv.config();
 
@@ -13,18 +12,16 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-// add routes here 
+// add routes here
 
 app.use(notFound);
 app.use(errorHandler);
 
-if (process.env.NODE_ENV !== 'production') {
-  const port = process.env.PORT || 8080;
+if (process.env.NODE_ENV !== "production") {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
